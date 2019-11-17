9c650da0940770ea1f10dd7292a7412cca1d6b09
Markus Strauss <mstrauss@users.noreply.github.com>
2019-11-17 03:47:00 +0100

    remove numpy 1.17.3 freeze

--- feature_mapper/wrapper.py
+++ feature_mapper/wrapper.py
@@ -7,15 +7,15 @@ from .feature_mapper import ffi, lib
 def C(spm):
     return dict(shape=spm.shape,
                 indices=(len(spm.indices),
-                         ffi.cast("int32_t *", spm.indices.ctypes.data)),
+                         ffi.from_buffer('int32_t *', spm.indices)),
                 indptr=(len(spm.indptr),
-                        ffi.cast("int32_t *", spm.indptr.ctypes.data)))
+                        ffi.from_buffer('int32_t *', spm.indptr)))
 
 
 def Py(c):
     return sparse.csr_matrix(
-        (np.ones(c.indices.len,
-                 dtype=np.bool), ffi.unpack(c.indices.vec, c.indices.len),
+        (np.ones(c.indices.len, dtype=np.bool), \
+         ffi.unpack(c.indices.vec, c.indices.len),
          ffi.unpack(c.indptr.vec, c.indptr.len)),
         shape=(c.shape.rows, c.shape.cols))
 
@@ -31,17 +31,17 @@ def sparsify(mat):
         return sparse.csr_matrix(np.array(mat, dtype=np.int8))
 
 
-def map_features(spm, im):
+def _v(spm, im):
     spm = C(sparsify(spm))
     im = C(sparsify(im))
     assert spm['shape'][1] == im['shape'][1],\
         "Number of columns of in-feature and mapping matrices must match."
-    return Py(lib.remap_rows(spm, im))
+    return spm, im
+
+
+def map_features(spm, im):
+    return Py(lib.remap_rows(*_v(spm, im)))
 
 
 def map_features_smin(spm, im, smin):
-    spm = C(sparsify(spm))
-    im = C(sparsify(im))
-    assert spm['shape'][1] == im['shape'][1],\
-        "Number of columns of in-feature and mapping matrices must match."
-    return Py(lib.remap_rows_smin(spm, im, smin))
+    return Py(lib.remap_rows_smin(*_v(spm, im), smin))
